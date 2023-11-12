cf5f690c5d893bb2890ddda14ecd9feb10be20da
Coding_WitipY <142069730+AgegoKai@users.noreply.github.com>
2023-11-12 18:52:16 +0100

    Update pc_side.py

--- pc_side.py
+++ pc_side.py
@@ -5,28 +5,31 @@ import time
 
 # Słownik przypisujący UID karty do imienia i nazwiska
 card_uid_to_name = {
-    "0X17DF5CB": "Jakub Gąsiorski",
+    "Card detected 017E018B": "Ania",
+    "Card detected 017BDE8B": "Jryna",
+    "Card detected 017E01BB": "Ola",
     # Dodaj więcej kart w formacie "UID": "Imię i nazwisko"
 }
 
 # Ustawienia portu szeregowego
-port = "COM11"
+port = "COM4"
 baudrate = 9600
 
 # Ustawienia połączenia z MySQL
 db_config = {
-    'host': 'localhost',
-    'user': 'twoj_uzytkownik',
-    'password': 'twoje_haslo',
-    'database': 'CardReadings'
+    'host': '',
+    'user': '',
+    'password': '',
+    'database': ''
 }
 
 def save_to_database(name, reading_time):
     try:
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
-        query = "INSERT INTO readings (name, reading_time) VALUES (%s, %s)"
-        cursor.execute(query, (name, reading_time))
+        query = f"INSERT INTO work_times (employee) VALUES ('{name}')"
+        print(query)
+        cursor.execute(query)
         conn.commit()
     except mysql.connector.Error as e:
         print(f"Błąd bazy danych: {e}")
@@ -49,11 +52,12 @@ def read_from_serial(ser):
     while True:
         try:
             if ser.in_waiting > 0:
-                uid = ser.readline().decode('utf-8').strip()
+                uid = str(ser.readline().decode('utf-8').strip())
                 name = card_uid_to_name.get(uid, "Nieznana karta")
                 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 print(f"{name} - Czas: {current_time}")
-                #save_to_database(name, current_time)
+                print(uid)
+                save_to_database(name, current_time)
         except serial.SerialException:
             print("Utracono połączenie z urządzeniem. Ponawiam próbę...")
             ser.close()
