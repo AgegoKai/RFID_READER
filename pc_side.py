50badb68c7dfede0e703ed15a2fc433dbccae952
Coding_WitipY <142069730+AgegoKai@users.noreply.github.com>
2023-11-12 16:49:22 +0100

    Update pc_side.py

--- pc_side.py
+++ pc_side.py
@@ -1,6 +1,7 @@
 import serial
 import datetime
 import mysql.connector
+import time
 
 # Słownik przypisujący UID karty do imienia i nazwiska
 card_uid_to_name = {
@@ -9,7 +10,7 @@ card_uid_to_name = {
 }
 
 # Ustawienia portu szeregowego
-port = "COM5"
+port = "COM11"
 baudrate = 9600
 
 # Ustawienia połączenia z MySQL
@@ -30,24 +31,35 @@ def save_to_database(name, reading_time):
     except mysql.connector.Error as e:
         print(f"Błąd bazy danych: {e}")
     finally:
-        cursor.close()
-        conn.close()
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
 
-def read_from_serial():
-    try:
-        ser = serial.Serial(port, baudrate, timeout=1)
-        print(f"Połączono z portem: {port}")
-        while True:
+def connect_to_serial():
+    while True:
+        try:
+            ser = serial.Serial(port, baudrate, timeout=1)
+            print(f"Połączono z portem: {port}")
+            return ser
+        except serial.SerialException:
+            print(f"Nie można połączyć z portem: {port}. Ponawiam próbę...")
+            time.sleep(5)  # Zamiast utime.sleep
+
+def read_from_serial(ser):
+    while True:
+        try:
             if ser.in_waiting > 0:
                 uid = ser.readline().decode('utf-8').strip()
                 name = card_uid_to_name.get(uid, "Nieznana karta")
                 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 print(f"{name} - Czas: {current_time}")
-                save_to_database(name, current_time)
-    except serial.SerialException as e:
-        print(f"Błąd połączenia: {e}")
-    finally:
-        ser.close()
+                #save_to_database(name, current_time)
+        except serial.SerialException:
+            print("Utracono połączenie z urządzeniem. Ponawiam próbę...")
+            ser.close()
+            return
 
 if __name__ == "__main__":
-    read_from_serial()
+    while True:
+        ser = connect_to_serial()
+        read_from_serial(ser)
